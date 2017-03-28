6e8746e6ff8732b409a1028459a18cb8d9b079af
Or Rikon <rikonor@gmail.com>
2017-03-28 11:34:18 -0400

    Add LocalhostOnly option (#1)

--- pprof_test.go
+++ pprof_test.go
@@ -22,3 +22,32 @@ func Test_routePrefix(t *testing.T) {
 		}
 	}
 }
+
+func Test_isLocalhost(t *testing.T) {
+	validTestCases := []string{
+		"localhost:12345",
+		"localhost",
+		"127.0.0.1:12345",
+		"127.0.0.1",
+		"[::1]:12345",
+		"[::1]",
+	}
+
+	for _, tc := range validTestCases {
+		if !isLocalhost(tc) {
+			t.Errorf("should be valid localhost, but is not: %s", tc)
+		}
+	}
+
+	invalidTestCases := []string{
+		"1.2.3.4",
+		"1.2.3.4:12345",
+		"fake-data",
+	}
+
+	for _, tc := range invalidTestCases {
+		if isLocalhost(tc) {
+			t.Errorf("should not be localhost, but is: %s", tc)
+		}
+	}
+}
