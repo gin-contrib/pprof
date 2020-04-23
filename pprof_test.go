3f090873d2a40dc014cfa797aa8d487c0309cb45
micanzhang <micanzhang@gmail.com>
2020-04-23 10:38:27 +0800

    add a new function RouteRegister which supports *gin.RouterGroup… (#20)
    first parameter

--- pprof_test.go
+++ pprof_test.go
@@ -1,6 +1,12 @@
 package pprof
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+)
 
 func Test_getPrefix(t *testing.T) {
 	tests := []struct {
@@ -16,5 +22,45 @@ func Test_getPrefix(t *testing.T) {
 		if got := getPrefix(tt.args...); got != tt.want {
 			t.Errorf("%q. getPrefix() = %v, want %v", tt.name, got, tt.want)
 		}
+	}
+}
+
+func TestRegisterAndRouteRegister(t *testing.T) {
+	bearerToken := "Bearer token"
+	gin.SetMode(gin.ReleaseMode)
+	r := gin.New()
+	Register(r)
+	adminGroup := r.Group("/admin", func(c *gin.Context) {
+		if c.Request.Header.Get("Authorization") != bearerToken {
+			c.AbortWithStatus(http.StatusForbidden)
+			return
+		}
+		c.Next()
+	})
+	RouteRegister(adminGroup, "pprof")
+
+	req, _ := http.NewRequest(http.MethodGet, "/debug/pprof/", nil)
+	rw := httptest.NewRecorder()
+	r.ServeHTTP(rw, req)
+
+	if expected, got := http.StatusOK, rw.Code; expected != got {
+		t.Errorf("expected: %d, got: %d", expected, got)
+	}
+
+	req, _ = http.NewRequest(http.MethodGet, "/admin/pprof/", nil)
+	rw = httptest.NewRecorder()
+	r.ServeHTTP(rw, req)
+
+	if expected, got := http.StatusForbidden, rw.Code; expected != got {
+		t.Errorf("expected: %d, got: %d", expected, got)
+	}
+
+	req, _ = http.NewRequest(http.MethodGet, "/admin/pprof/", nil)
+	req.Header.Set("Authorization", bearerToken)
+	rw = httptest.NewRecorder()
+	r.ServeHTTP(rw, req)
+
+	if expected, got := http.StatusOK, rw.Code; expected != got {
+		t.Errorf("expected: %d, got: %d", expected, got)
 	}
 }
